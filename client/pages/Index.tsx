a9e6f63e01a7bfb6f5e6a00badd7a04fdbcd26e4
Ajai-3 <ajaipjayan@gmail.com>
2025-07-06 20:55:27 +0530

    page style refactore

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Timer } from "@/components/Timer";
-import { Analytics } from "@/components/Analytics";
+impornalytics } from "@/components/Analytics";
 import { SessionList } from "@/components/SessionList";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
