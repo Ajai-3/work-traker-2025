c380573a5a2ae130034cb7d618462c5392b754bb
Ajai-3 <ajaipjayan@gmail.com>
2025-07-09 21:50:43 +0530

    refactor

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,7 +1,3 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-
-  Error: User attempted to access non-existent route:",
-      location.pathname,
-   
-export default NotFound;
+efault NotFound;
