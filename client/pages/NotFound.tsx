b6572c04513d395c555b7cec422a6bf2cde52c46
Ajai-3 <ajaipjayan@gmail.com>
2025-07-10 22:47:24 +0530

    refactor

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,6 +1,8 @@
 import { u
 Error: User attempted to access non-existent route:",
-      on.pathname]);
+      location.pathname
+    );
+  }, [location.pathname]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
