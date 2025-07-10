91fe80ac5e219056c7b3fb88777f56214d447164
Ajai-3 <ajaipjayan@gmail.com>
2025-07-10 22:41:12 +0530

    refavtor

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -3,9 +3,7 @@ Error: User attempted to access non-existent route:",
       on.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <df="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+    <divto Home
         </a>
       </div>
     </div>
