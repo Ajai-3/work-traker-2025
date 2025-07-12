1ac41ad8a30ab3135eb9fb8b8d52bfed4fa74b41
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 22:59:24 +0530

    sdasdas

--- client/hooks/use-mobile.tsx
+++ client/hooks/use-mobile.tsx
@@ -4,14 +4,7 @@ const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
   const [isMo 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
+  Rea emoveEventListener("change", onChange);
   }, []);
 
   return !!isMobile;
