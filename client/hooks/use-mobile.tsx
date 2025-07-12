d07c0f9551a2005e3fa1fb2962d01334e79c5191
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 22:59:30 +0530

    sdasdas

--- client/hooks/use-mobile.tsx
+++ client/hooks/use-mobile.tsx
@@ -2,10 +2,4 @@ import * as React from "react";
 
 const MOBILE_BREAKPOINT = 768;
 
-export function useIsMobile() {
-  const [isMo 
-  Rea emoveEventListener("change", onChange);
-  }, []);
-
-  return !!isMobile;
-}
+export function useIsMobile() {}
