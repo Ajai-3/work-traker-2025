b3d33e76266949b46969b786595e8030ad287d60
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 22:58:43 +0530

    sdasdas

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -4,13 +4,7 @@ import { useEffect } from "react";
 consonsole.error(
       "404 Error: User attempted to access non-existent route:",
       location.pathname
-    );
-  }, [location.pathname]);
-
-  return (text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+    ); 
         </a>
       </div>
     </div>
