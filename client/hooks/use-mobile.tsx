ffe2fea2432944a502eddd01a51c6d04a6368bdf
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 23:00:52 +0530

    sdasdas

--- client/hooks/use-mobile.tsx
+++ client/hooks/use-mobile.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768;
-
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined
-  );
+c 
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
