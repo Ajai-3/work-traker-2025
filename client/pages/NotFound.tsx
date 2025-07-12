f7e95028961fee85b50d1921dc52adeb9e3223f7
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 22:58:59 +0530

    sdasdas

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,11 +1,2 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-
-consonsole.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    ); 
-        </a>
-      </div> 
-
-export default NotFound;
