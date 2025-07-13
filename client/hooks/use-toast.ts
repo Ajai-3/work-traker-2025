891bcbd618948d73d842d0c13de5299285fcdf45
Ajai-3 <ajaipjayan@gmail.com>
2025-07-13 21:05:30 +0530

    asdasd

--- client/hooks/use-toast.ts
+++ client/hooks/use-toast.ts
@@ -1,14 +1,7 @@
 import * as React from "react";
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
-asdasd
-    asdasd
-
-  return {
-    ...state,
-    toast,
-    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
-  };
-}
+asdasd;
+asdasd;
 
 export { useToast, toast };
