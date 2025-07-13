9d3ec30c00b487159b4e3b2b8ed6939d5d1b66e6
Ajai-3 <ajaipjayan@gmail.com>
2025-07-13 21:04:59 +0530

    asdasdasd

--- client/hooks/use-toast.ts
+++ client/hooks/use-toast.ts
@@ -3,14 +3,7 @@ import * as React from "react";
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
-
-type ToasterToast = ToastProps & {
-  id: string;
-  title?: React.ReactNode;
-  description?: React.ReactNode;
-  aczxczxczxc;
-};zczxc
+asdasdasd
   const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id });
 
   dispatch({
